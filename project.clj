844dcdbf10b89f9e544c4ceac7c8c3c827793eb4
Kazuo Koga <obiwanko@me.com>
2015-05-11 23:31:49 +0900

    change homepage

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject adhoc-plist "1.0.0"
   :description "Generate .plist file with AdHoc .ipa package for OTA."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/kkazuo/adhoc-plist"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
