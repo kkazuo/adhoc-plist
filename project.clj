f30d39d31f4144a86d5f05c0a7dc8b482c9de1af
Kazuo Koga <obiwanko@me.com>
2015-05-11 23:49:01 +0900

    bump version for fix clojar

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject adhoc-plist "1.0.0"
+(defproject adhoc-plist "1.0.1"
   :description "Generate .plist file with AdHoc .ipa package for OTA."
   :url "https://github.com/kkazuo/adhoc-plist"
   :license {:name "Eclipse Public License"
