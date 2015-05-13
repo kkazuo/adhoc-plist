b7cf48612412a66fd1304fdb8661939092a36a8e
Kazuo Koga <obiwanko@me.com>
2015-05-13 23:43:31 +0900

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject adhoc-plist "1.0.1"
+(defproject adhoc-plist "1.0.2"
   :description "Generate .plist file with AdHoc .ipa package for OTA."
   :url "https://github.com/kkazuo/adhoc-plist"
   :license {:name "Eclipse Public License"
